0f11b494b591fef6d49135fb67aae43188e44e0d
burhanmubarok@gmail.com <burhanmubarok@gmail.com>
2016-07-18 12:30:31 +0700

    Initial

---
+++ ConsoleApplication/ConsoleApplication/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApplication
+{
+    public class CartEntry2
+    {
+        public float price;
+        public int quantity;
+
+        public float GetTotal()
+        {
+            return price*quantity;
+        }
+    }
+
+    public class CartContent2
+    {
+        public CartEntry2[] items;
+
+        public float TotalInEntry()
+        {
+            float total = 0;
+            foreach (CartEntry2 item in items)
+            {
+                total += item.GetTotal();
+            }
+            return total;
+        }
+    }
+    public class Order2
+    {
+        public CartContent2 cart;
+        public float tax;
+        public Order2(CartContent2 cart, float tax)
+        {
+            this.cart = cart;
+            this.tax = tax;
+        }
+
+        public float OrderTotal()
+        {
+            return cart.TotalInEntry();
+        }
+    }
+}
---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private CartEntry2[] items;
+
+            CartContent2 orderList = new CartContent2();
+            Order2 newOrder = new Order2(orderList, 3);
+            Console.Write(newOrder);
+            Console.ReadKey();
+        }
+    }
+
+    public class CartEntry
+    {
+        public float price;
+        public int quantity;
+    }
+
+    public class CartContent
+    {
+        public CartEntry[] items;
+    }
+    public class Order
+    {
+        public CartContent cart;
+        public float tax;
+        public Order(CartContent cart, float tax)
+        {
+            this.cart = cart;
+            this.tax = tax;
+        }
+
+        public float OrderTotal()
+        {
+            float total = 0;
+            for (int i = 0; i < cart.items.Length; i++)
+            {
+                total += cart.items[i].price*cart.items[i].quantity;
+            }
+            total += total*tax;
+            return total;
+        }
+    }
+}
